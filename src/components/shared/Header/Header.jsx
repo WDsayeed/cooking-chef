eaff0fc19f230e75df4760e422667d1d61378740
WDsayeed <sayeedahammed526@gmail.comm>
2023-05-01 23:39:17 +0600

    added header section

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-        return (
-                <div>
-                        this is header
-                </div>
-        );
+  return (
+    <div className="navbar bg-base-100 md:container md:mx-auto">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-4xl">Cooking<span className="text-purple-700">Master</span></a>
+      </div>
+      <div className="">
+        <a className="btn btn-ghost normal-case text-gray-600 text-lg">Home</a>
+      </div>
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-gray-600 text-lg">Blog</a>
+      </div>
+      <div className="flex-none gap-2">  
+            <div className="w-10 rounded-full">
+              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
