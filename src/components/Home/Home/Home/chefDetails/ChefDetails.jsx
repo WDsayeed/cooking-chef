22601016b3a886a86ccb7e1bc206ee0ee90eccf5
WDsayeed <sayeedahammed526@gmail.comm>
2023-05-02 16:36:37 +0600

    added details

--- src/components/Home/Home/Home/chefDetails/ChefDetails.jsx
+++ src/components/Home/Home/Home/chefDetails/ChefDetails.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
 import Details from '../details/Details';
 
 const ChefDetails = () => {
-        const details = useLoaderData()
-        console.log(details)
+        const {id} = useParams()
+        console.log(id)
+        const [details, setDetails] = useState([])
+        useEffect( ()=>{
+                fetch(`http://localhost:5000/chef`)
+                .then(res=> res.json())
+                .then(data=> setDetails(data))
+        },[])
+
+        const filterData = details.find(dt=> dt.id == id)
+        console.log(filterData)
         return (
                 <div>
                         <h2>chef details </h2>
--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       {
         path:"/details/:id",
         element:<ChefDetails></ChefDetails>,
-        loader: ({params})=> fetch(`http://localhost:5173/details/${params.id}`)
+        // loader: ()=> fetch("http://localhost:5000/chef")
       },
       {
         path: "login",
