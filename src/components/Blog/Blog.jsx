07156ea389205da9ba68bbd9f2e3cfed6048f60f
WDsayeed <sayeedahammed526@gmail.comm>
2023-05-04 07:23:03 +0600

    added fun pdf

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-
-
+import ReactToPdf from "react-to-pdf";
+const ref = React.createRef();
 const Blog = () => {
   return (
     <div>
-    <ReactToPdf targetRef={ref} filename="div-blue.pdf" options={options} x={.5} y={.5} scale={0.8}>
+    <ReactToPdf targetRef={ref} filename="blog.pdf">
         {({toPdf}) => (
             <button onClick={toPdf}>Generate pdf</button>
         )}
     </ReactToPdf>
     <div className="container mx-auto my-11"  ref={ref}>
     
-      <div  className="mb-4">
+      <div className="mb-4">
         <h1 className="text-xl font-semibold mb-2">
           1. Differences between uncontrolled and controlled components
         </h1>
@@ -73,48 +73,6 @@ const Blog = () => {
         </p>
       </div>
     </div>
-
-
-
-{/* navber */}
-    <div className="navbar-start md:container md:mx-auto">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-
-      
-      
-      <li className="mr-4">
-        {/* <Link to="/blog" className=" text-gray-600 text-lg">Blog</Link> */}
-        <NavLink to="/register"
-        className={({ isActive }) => (isActive ? "text-purple-950 font-semibold" : '')}
-        >
-          Register
-        </NavLink>
-      </li>
-      </ul>
-      <div className="">
-        <h1 className="font-semibold text-4xl">Cooking<span className="text-purple-700">Master</span></h1>
-      </div>
-      
-     
-     
-      {
-        user && <div className="md:flex-none gap-2 mr-2 ">  
-        <div  className="w-10 rounded-full "  >
-          <img data-tooltip-target="tooltip-default"  src={user?.photoURL} />
-        </div>
-  </div>
-      }
-      {
-        user ? <button onClick={handleLogOut} className=" text-white w-16 h-10 rounded  px-2 bg-purple-600">Logout</button>:
-        <Link to="/login">
-        <button className=" text-white w-16 h-10 rounded  px-2 bg-purple-600">Login</button>
-        </Link>
-      }
-    </div>
-    {/* nav */}
     </div>
 
   );
